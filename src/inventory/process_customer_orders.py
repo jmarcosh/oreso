466fe4a3ddd81f52968d6c8dbec6426e7bb4d874
jmarcosh <jmarcosh@gmail.com>
2026-09-14 22:35:31 +0300

    sort checklist by C.SECTION

--- src/inventory/process_customer_orders.py
+++ src/inventory/process_customer_orders.py
@@ -179,7 +179,7 @@ def sort_rd(rd):
 def create_po_summary_by_style(po, config):
     po_gb = po.groupby(config['po_style_indexes'], dropna=False).agg(config['po_style_values']).reset_index()
     po_gb = po_gb.sort_values(
-        by=[C.RD, C.WAREHOUSE_CODE],
+        by=[C.SECTION, C.RD, C.WAREHOUSE_CODE],
         key=lambda col: col.map(sort_rd) if col.name == 'code' else col,
         ignore_index=True
     )
